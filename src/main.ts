30d7fd8c3432bdda67723966131cc23ed8113f22
kessleru <otaviokesslerustra@hotmail.com>
2026-06-11 23:36:50 -0300

    feat: add normalization function and transaction types for data processing

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,17 @@
 // import './style.css';
 import fetchData from './fetchData';
-
-type TransacaoPagamento = 'Boleto' | 'Cartão de Crédito';
-type TransacaoStatus =
-  | 'Paga'
-  | 'Recusada pela operadora de cartão'
-  | 'Aguardando pagamento'
-  | 'Estornada';
-
-interface TransacaoAPI {
-  Nome: string;
-  ID: number;
-  Data: string;
-  Status: TransacaoStatus;
-  Email: string;
-  ['Valor (R$)']: string;
-  ['Forma de Pagamento']: TransacaoPagamento;
-  ['Cliente Novo']: number;
-}
+import normalizarTransacao from './normalizarTransacao';
 
 async function handleData() {
   const data = await fetchData<TransacaoAPI[]>('https://api.origamid.dev/json/transacoes.json');
 
+  if(!data) return;
+
+  const transacoes = data.map(normalizarTransacao)
+
   if (data) {
     data.forEach((item) => {
-      console.log(item);
+      console.log(item['Valor (R$)']);
     });
   }
 }
